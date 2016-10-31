2b8f10a100471c33fca9f32bc8cd6b8752626aa5
Sean MacPherson <smacpher@nestlabs.com>
2016-10-31 00:11:27 -0700

    Added signal handling for keyboard interrupts; TODO: add error handling.

--- main.cpp
+++ main.cpp
@@ -3,78 +3,36 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <signal.h>
 #include <sstream>
 #include <stdexcept>
+#include <stdlib.h>
 #include <string>
 
 using namespace std;
 
-bool fileExists(const string&);
+const string SESSION_FILENAME = ".session.cpp";
+const string SESSION_BINARY_NAME = ".session";
+
+bool checkEmpty(string);
 bool checkExit(string);
+bool fileExists(const string&);
+bool isGlobal(string);
 ofstream initSession();
 string compile(string, string);
 string exec(string);
 string run(string);
-void finalizeSession(string);
 string strip(string&);
-bool isGlobal(string);
+void finalizeSession(string);
+void handleKeyboardInterrupt(int);
 
-// var& passes the file reference (reference to the location in memory where
-// the file object is stored)
-void initSession(string filename, ofstream& file) {
-    const bool file_exists = fileExists(filename);
-
-    if (!file_exists) {
-        cout << "Initializing session..." << endl;
-        ofstream init_file(filename.c_str());
-        if (!init_file.is_open()) {
-            cerr << "Could not initialize session." << endl;
-            return;
-        }
-        cout << "Successfully initialized session." << endl;
-    }
-
-    file.open(filename.c_str());
+// Signal handlers.
+void handleKeyboardInterrupt(int sig) {
+    finalizeSession(SESSION_FILENAME);
+    exit(sig);
 }
 
-bool fileExists(const string& filename) {
-    ifstream f(filename.c_str());
-    return f.is_open();
-}
-
-void finalizeSession(string filename) {
-    ofstream session(filename.c_str(), ios::out | ios::trunc);
-    session.close();
-}
-
-string exec(string cmdStr) {
-    const char * cmd = cmdStr.c_str();
-    char buffer[128];
-    string result = "";
-    shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
-        throw runtime_error("popen() failed.");
-    }
-    while (!feof(pipe.get())) {
-        if (fgets(buffer, 128, pipe.get()) != NULL) {
-            result += buffer;
-        }
-    }
-    return result;
-}
-
-bool checkExit(string sessionIn) {
-    transform(sessionIn.begin(), sessionIn.end(), sessionIn.begin(), ::toupper);
-    if (sessionIn == "EXIT" || 
-        sessionIn == "QUIT" ||
-        sessionIn == "EXIT()" ||
-        sessionIn == "QUIT()") {
-        return true;
-    } else {
-        return false;
-    }
-}
-
+// Utils.
 bool checkEmpty(string s) {
     string stripped_s;
     stripped_s = strip(s);
@@ -85,24 +43,21 @@ bool checkEmpty(string s) {
     }
 }
 
-string compile(string fileName, string binName) {
-    string cmd;
-    cmd = "g++ -o " + binName + " " + fileName;
-    return exec(cmd);
+bool checkExit(string sessionIn) {
+    transform(sessionIn.begin(), sessionIn.end(), sessionIn.begin(), ::toupper);
+    if (sessionIn == "EXIT" ||
+        sessionIn == "QUIT" ||
+        sessionIn == "EXIT()" ||
+        sessionIn == "QUIT()") {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-string run(string binName) {
-    string cmd;
-    cmd = "./" + binName;
-    return exec(cmd);
-}
-
-void constructSession(string filename, string globalBody, string mainBody) {
-    ofstream session(filename, ios::out | ios::trunc);
-    session << globalBody << endl;
-    session << "int main()" << endl << "{" << endl;
-    session << mainBody << endl;
-    session << "return 0;" << endl << "}" << endl;
+bool fileExists(const string& filename) {
+    ifstream f(filename.c_str());
+    return f.is_open();
 }
 
 bool isGlobal(string s) {
@@ -121,7 +76,7 @@ bool isGlobal(string s) {
     return false;
 }
 
-// TODO: deal with tabs, too.
+// TODO: Handle trailing and leading tabs, too.
 string strip(string& str) {
     size_t first = str.find_first_not_of(' ');
     size_t last = str.find_last_not_of(' ');
@@ -134,9 +89,68 @@ string strip(string& str) {
     return str.substr(first, (last - first + 1));
 }
 
+// Build/Teardown Session.
+void initSession(string filename, ofstream& file) {
+    const bool file_exists = fileExists(filename);
+
+    if (!file_exists) {
+        cout << "Initializing session..." << endl;
+        ofstream init_file(filename.c_str());
+        if (!init_file.is_open()) {
+            cerr << "Could not initialize session." << endl;
+            return;
+        }
+        cout << "Successfully initialized session." << endl;
+    }
+
+    file.open(filename.c_str());
+}
+
+void constructSession(string filename, string globalBody, string mainBody) {
+    ofstream session(filename, ios::out | ios::trunc);
+    session << globalBody << endl;
+    session << "int main()" << endl << "{" << endl;
+    session << mainBody << endl;
+    session << "return 0;" << endl << "}" << endl;
+}
+
+void finalizeSession(string filename) {
+    ofstream session(filename.c_str(), ios::out | ios::trunc);
+    session.close();
+}
+
+// Execute session.
+string exec(string cmdStr) {
+    const char * cmd = cmdStr.c_str();
+    char buffer[128];
+    string result = "";
+    shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw runtime_error("popen() failed.");
+    }
+    while (!feof(pipe.get())) {
+        if (fgets(buffer, 128, pipe.get()) != NULL) {
+            result += buffer;
+        }
+    }
+    return result;
+}
+
+string compile(string fileName, string binName) {
+    string cmd;
+    cmd = "g++ -o " + binName + " " + fileName;
+    return exec(cmd);
+}
+
+string run(string binName) {
+    string cmd;
+    cmd = "./" + binName;
+    return exec(cmd);
+}
+
 int main() {
-    const string SESSION_FILENAME = ".session.cpp";
-    const string SESSION_BINARY_NAME = ".session";
+    // Register signals.
+    signal(SIGINT, handleKeyboardInterrupt);
 
     ofstream session;
     ostringstream sessionMain;
@@ -147,7 +161,7 @@ int main() {
     initSession(SESSION_FILENAME, session);
 
     while(true) {
-        // First, fetch current session.
+        // Fetch current session.
         if (session.is_open()) {
             // Update session.
             cout << "[In]: ";
@@ -176,7 +190,7 @@ int main() {
             break;
         }
     }
-    // finalizeSession(SESSION_FILENAME);
+    finalizeSession(SESSION_FILENAME);
     return 0;
 }
 
